4066d3b009d99d9b61b2c0a688326c8fe4cb07b1
Yesenia Torres <yesentorres@gmail.com>
2025-04-10 14:50:11 -0700

    add new lens images to display for new anniversary lenses

--- src/hooks/useLensColors.tsx
+++ src/hooks/useLensColors.tsx
@@ -29,12 +29,22 @@ export const useLensColors = () => {
             gatsbyImageData(quality: 40, height: 55)
           }
         }
+        g15_gradient: file(relativePath: { eq: "g15-gradient-lens.png" }) {
+          childImageSharp {
+            gatsbyImageData(quality: 40, height: 55)
+          }
+        }
         clear: file(relativePath: { eq: "clear-lens.png" }) {
           childImageSharp {
             gatsbyImageData(quality: 40, height: 55)
           }
         }
         yellow: file(relativePath: { eq: "yellow-lens.png" }) {
+          childImageSharp {
+            gatsbyImageData(quality: 40, height: 55)
+          }
+        }
+        burnt_orange: file(relativePath: { eq: "burnt-orange-lens.png" }) {
           childImageSharp {
             gatsbyImageData(quality: 40, height: 55)
           }

99a2c06054d61228bd327781699175a1a76014af
j-Riv <jose.alfredo.rivera@gmail.com>
2025-04-30 11:23:43 -0700

    refactor: Added new handler that does not wait for response from lambda, this is useful since netlify functions have a timeout of 10 seconds. I have requested an increase.

--- src/api/newDealerApplication.ts
+++ src/api/newDealerApplication.ts
@@ -33,6 +33,38 @@ function createHmacSha256Hash(data: string): string {
   return hmac
 }
 
+// uncomment this function to use without waiting for the response
+// this is due to the standard 10 second timeout for netlify functions
+// export default async function newDealerApplication(
+//   req: GatsbyFunctionRequest,
+//   res: GatsbyFunctionResponse
+// ) {
+//   try {
+//     const { body } = req
+//     console.log("Request body:", body)
+//     if (!body) {
+//       throw new Error("No body found")
+//     }
+
+//     const endpoint = process.env.TN_DEALER_APPLICATION_ENDPOINT as string
+//     const data = JSON.stringify(body)
+//     fetch(endpoint, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "X-Tres-Noir-Hmac-Sha256": createHmacSha256Hash(data),
+//       },
+//       body: data,
+//     })
+//     res.status(200).json({ message: "ok" })
+//   } catch (error) {
+//     console.log("Error Creating PDF:", error)
+//     res.status(500).json({ error: "Error creating PDF" })
+//   }
+// }
+
+// uncomment this function to use with waiting for the response
+// netlify functions have a standard 10 second timeout, but you can request an increase to 30 seconds on a paid plan
 export default async function newDealerApplication(
   req: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse

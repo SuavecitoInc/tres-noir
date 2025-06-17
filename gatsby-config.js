802b0032457432bd6d62043a3485dc1fbfdd6f99
j-Riv <jose.alfredo.rivera@gmail.com>
2025-06-17 15:23:31 -0700

    chore: Moved Contentful Environment to a env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const contentfulConfig = {
   accessToken:
     process.env.CONTENTFUL_ACCESS_TOKEN ||
     process.env.CONTENTFUL_DELIVERY_TOKEN,
-  environment: "staging",
+  environment: process.env.CONTENTFUL_ENVIRONMENT || "staging",
 }
 
 const siteUrl = `https://www.tresnoir.com`

fd77bde2e441acaee18463b836fa453d71f928de
j-Riv <jose.alfredo.rivera@gmail.com>
2025-09-18 12:29:46 -0700

    refactor: Added new suggested addons popup based on the upsell component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -190,7 +190,7 @@ const Header = ({
 
   return (
     <Component>
-      <BannerMessage />
+      {/* <BannerMessage /> */}
       <div className="top-wrapper">
         {isIndex ? (
           <h1 className="h1">

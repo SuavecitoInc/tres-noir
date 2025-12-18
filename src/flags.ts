abc66f3123987f38726ac72a0573f25202315cad
j-Riv <jose.alfredo.rivera@gmail.com>
2025-12-18 12:20:04 -0800

    feature: Added feature flags to enable and use the new Shopify Customer Accounts API instead of the classic account.

---
+++ src/flags.ts
@@ -0,0 +1,5 @@
+// use this flag to enable the new Shopify customer accounts (not classic)
+export const ENABLE_NEW_CUSTOMER_ACCOUNTS = true
+
+// use this flag to enable the forwarding to the new Shopify customer accounts page instead of the Gatsby one
+export const FORWARD_TO_NEW_CUSTOMER_ACCOUNTS = false

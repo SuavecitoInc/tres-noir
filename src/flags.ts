b4bb56fbf4c8edeacdeefbce3939bad31ac934ae
j-Riv <jose.alfredo.rivera@gmail.com>
2026-01-05 09:00:36 -0800

    chore: Preview in Netlify Production

--- src/flags.ts
+++ src/flags.ts
@@ -1,5 +1,5 @@
 // use this flag to enable the new Shopify customer accounts (not classic)
-export const ENABLE_NEW_CUSTOMER_ACCOUNTS = true
+export const ENABLE_NEW_CUSTOMER_ACCOUNTS = false
 // use this flag to enable the forwarding to the new Shopify customer accounts page instead of the Gatsby one
 export const FORWARD_TO_NEW_CUSTOMER_ACCOUNTS = false
 // use this flag to enable prescription file uploads

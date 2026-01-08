782ca5dd7a276a988a5456343b7bc67c5b5f5c69
j-Riv <jose.alfredo.rivera@gmail.com>
2026-01-08 11:06:36 -0800

    chore: Added Customization Exclusions

---
+++ src/data/exclusions.ts
@@ -0,0 +1,13 @@
+export const CUSTOMIZATION_EXCLUSIONS = {
+  SAFETY_GLASSES: [
+    "TN-PS-007",
+    "TN-PS-008",
+    "TN-PS-009",
+    "TN-PS-010",
+    "TN-PS-011",
+    "TN-PS-012",
+    "TN-PS-013",
+    "TN-PS-014",
+    "TN-PS-015",
+  ],
+}

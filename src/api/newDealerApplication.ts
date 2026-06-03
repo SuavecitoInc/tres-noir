58d09e15aca906da10810e2a34adf7ec50ff9205
j-Riv <jose.alfredo.rivera@gmail.com>
2026-06-03 10:02:38 -0700

    refactor: Updated the Dealer Application to post to the Queue Server and insert a new Lead into NetSuite.

--- src/api/newDealerApplication.ts
+++ src/api/newDealerApplication.ts
@@ -2,15 +2,20 @@ import type { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby"
 import fetch from "node-fetch"
 import * as crypto from "crypto"
 interface FormData {
+  subsidiary: number
   storeName: string
+  email: string
   storeTel: string
   address: string
+  address2?: string
   city: string
   stateProvince: string
   postalCode: string
   country: string
-  owners: string
-  authorizedBuyerContact: string
+  ownerFirstName: string
+  ownerLastName: string
+  authorizedBuyerFirstName: string
+  authorizedBuyerLastName: string
   businessType: string
   question1: string
   averageRetailPrice: string
@@ -23,48 +28,45 @@ interface FormData {
   date: string
 }
 
+// type NetSuiteLeadResponse = {
+//   success: boolean
+//   data: {
+//     id: number
+//   } | null
+//   error?: null | string
+// }
+
 // create a function to create an hmac sha256 hash
 function createHmacSha256Hash(data: string): string {
-  const secret = process.env.SECRET_KEY as string
+  // const secret = process.env.SECRET_KEY as string
+  const secret = process.env.SUPER_SECRET as string
   if (!secret) {
-    throw new Error("SECRET_KEY is not defined")
+    throw new Error("SUPER_SECRET is not defined")
   }
   const hmac = crypto.createHmac("sha256", secret).update(data).digest("base64")
   return hmac
 }
 
-// uncomment this function to use without waiting for the response
-// this is due to the standard 10 second timeout for netlify functions
-// export default async function newDealerApplication(
-//   req: GatsbyFunctionRequest,
-//   res: GatsbyFunctionResponse
-// ) {
-//   try {
-//     const { body } = req
-//     console.log("Request body:", body)
-//     if (!body) {
-//       throw new Error("No body found")
-//     }
+async function createNetSuiteLead(data: FormData) {
+  const endpoint = process.env.API_ENDPOINT as string
 
-//     const endpoint = process.env.TN_DEALER_APPLICATION_ENDPOINT as string
-//     const data = JSON.stringify(body)
-//     fetch(endpoint, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "X-Tres-Noir-Hmac-Sha256": createHmacSha256Hash(data),
-//       },
-//       body: data,
-//     })
-//     res.status(200).json({ message: "ok" })
-//   } catch (error) {
-//     console.log("Error Creating PDF:", error)
-//     res.status(500).json({ error: "Error creating PDF" })
-//   }
-// }
+  // patch data with subsidiary
+  data.subsidiary = 5
 
-// uncomment this function to use with waiting for the response
-// netlify functions have a standard 10 second timeout, but you can request an increase to 30 seconds on a paid plan
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Suavecito-Hmac-Sha256": createHmacSha256Hash(JSON.stringify(data)),
+    },
+    body: JSON.stringify(data),
+  })
+
+  const responseJson = (await response.json()) as { id: number }
+  console.log("Response from NetSuite lead endpoint:", responseJson)
+
+  return responseJson
+}
 
 export default async function newDealerApplication(
   req: GatsbyFunctionRequest,
@@ -77,26 +79,16 @@ export default async function newDealerApplication(
       throw new Error("No body found")
     }
 
-    const endpoint = process.env.TN_DEALER_APPLICATION_ENDPOINT as string
-    const data = JSON.stringify(body)
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Tres-Noir-Hmac-Sha256": createHmacSha256Hash(data),
-      },
-      body: data,
-    })
+    const response = await createNetSuiteLead(body as FormData)
 
-    const responseJson: any = await response.json()
-    console.log("Response from endpoint:", responseJson)
-    if (responseJson.error) {
-      console.log("Error from endpoint:", responseJson.error)
-      return res.status(403).json({ error: responseJson.error })
+    if (!response.id) {
+      console.log("Error creating lead, no ID returned:", response)
+      return res.status(403).json({ error: "Error creating lead" })
     }
+
     res.status(200).json({ message: "ok" })
   } catch (error) {
-    console.log("Error Creating PDF:", error)
-    res.status(500).json({ error: "Error creating PDF" })
+    console.log("Error Creating Lead:", error)
+    res.status(500).json({ error: "Error creating Lead" })
   }
 }
